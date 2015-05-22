c19bfa53b0169fc1827fd96920b9548c772eced7
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-05-22 13:01:45 -0300

    Criação da funcionalidade para determinar o menor lance na avaliação do leilão

--- src/main/java/br/com/mvabiguzzi/testesAutomatizados/leilao/TesteDoAvaliador.java
+++ src/main/java/br/com/mvabiguzzi/testesAutomatizados/leilao/TesteDoAvaliador.java
@@ -3,6 +3,7 @@ package br.com.mvabiguzzi.testesAutomatizados.leilao;
 public class TesteDoAvaliador {
 	
 	public static void main(String[] args) {
+		//Cenario 1: 3 lances nao ordenados
 		Usuario joao = new Usuario("Joao");
 		Usuario jose = new Usuario("Jose");
 		Usuario maria = new Usuario("Maria");
@@ -17,6 +18,20 @@ public class TesteDoAvaliador {
 		leiloeiro.avalia(leilao);
 		
 		System.out.println(leiloeiro.getMaiorDeTodos());
+		System.out.println(leiloeiro.getMenorDeTodos());
+		
+		//Cenario 2: 3 lances em ordem crescente
+		Leilao leilao1 = new Leilao("Playstation 3 novo");
+		
+		leilao1.propoe(new Lance(maria, 250.0));
+		leilao1.propoe(new Lance(joao, 300.0));
+		leilao1.propoe(new Lance(jose, 400.0));
+		
+		Avaliador leiloeiro1 = new Avaliador();
+		leiloeiro1.avalia(leilao1);
+		
+		System.out.println(leiloeiro1.getMaiorDeTodos());
+		System.out.println(leiloeiro1.getMenorDeTodos());
 	}
 	
 }
